d011f40b85cdcf9ea612302d1a6f1c5473ca858e
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-02-21 16:21:16 +0100

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+# TODO: Write a seed to insert 100 posts in the database
+100.times do
+  Article.create(title: Faker::Book.title, content: Faker::Book.author)
+end
